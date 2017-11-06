a8378fcb6984c942ed01b5a178a66a2adb347a68
habeeb <habibmail31@yahoo.com>
2017-11-06 06:09:02 +0100

    new update

--- mongoose.js
+++ mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 var config = require("./config/config")
 mongoose.Promise = global.Promise;
+// mongoose.connect(config.dbURL, { useMongoClient: true }, function (err) {
+// });
 mongoose.connect("mongodb://guest:guest@ds037215.mlab.com:37215/kaystore", { useMongoClient: true},function(err){
 });
-// mongoose.createConnection("mongodb://habeebtheprogrammer:k1o2l3a4@ds037215.mlab.com:37215/kaystore")
 module.exports = mongoose;
